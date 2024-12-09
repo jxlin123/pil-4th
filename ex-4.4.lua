3a0c809a143437c11e36760722c3f52136591304
jxlin123 <42682760+jxlin123@users.noreply.github.com>
2024-12-08 22:16:01 -0800

    Fix wrong indices in 4.4 and 4.6 exercise answers
    As discussed on <https://github.com/oitofelix/pil-4th/issues/2>, the
    original solutions for exercises 4.4 and 4.6 may accidentally "cut off"
    bytes, given the nature of Unicode codepoints possibly being encoded
    using multiple bytes. I've now gone ahead and applied the fix as
    described in that link.

--- ex-4.4.lua
+++ ex-4.4.lua
@@ -6,10 +6,27 @@
 
 function insert (to, i, from)
    return to:sub (1,
-		  utf8.offset (to, i - 1))
+                  -- Notice how we find the position of the Unicode
+                  -- codepoint _at_ the location we want to insert,
+                  -- and then subtract 1, as opposed to just finding
+                  -- the position of the preceding codepoint. This is
+                  -- because codepoints can span multiple bytes, and
+                  -- simply doing `utf8.offset(to, i - 1)` has the
+                  -- potential to cut off bytes.
+		  utf8.offset (to, i) - 1)
       .. from
       .. to:sub (utf8.offset (to, i),
 		 -1)
 end
 
 insert ("ação", 5, "!") --> ação!
+
+-- Japanese example.
+jp = '私は日本語が分かります。'
+-- print(jp)
+print(insert(jp, 3, "少し")) --> 私は少し日本語が分かります。
+
+-- Chinese example.
+zh = "我學了四年了，可是很長的時間沒有練習。"
+-- print(zh)
+print(insert(zh, 4, "中文")) --> 我學了中文四年了，可是很長的時間沒有練習
--- ex-4.6.lua
+++ ex-4.6.lua
@@ -6,12 +6,23 @@
 -- in codepoints.)
 
 function remove (s, i, n)
-   return s:sub (1,
-		 utf8.offset (s,
-			      i - 1))
-      .. s:sub (utf8.offset (s,
-			     i + n),
-		-1)
+   -- Notice how we find the position of the Unicode codepoint _at_
+   -- the place where we want to start removing characters, and then
+   -- subtract 1 to get the byte before. Had we simply done something
+   -- like `utf8.offset(s, i - 1)`, we would've cut off any remaining
+   -- bytes from the (i - 1)th Unicode codepoint
+   return s:sub (1, utf8.offset (s, i) - 1)
+      .. s:sub (utf8.offset (s, i + n), -1)
 end
 
 remove ("ação", 2, 2) --> ao
+
+-- Japanese example.
+jp = '私は少し日本語が分かります。'
+-- print(jp)
+print(remove(jp, 3, 2)) --> 私は日本語が分かります。
+
+-- Chinese example.
+zh = "我學了中文四年了，可是很長的時間沒有練習。"
+-- print(zh)
+print(remove(zh, 4, 2)) --> 我學了四年了，可是很長的時間沒有練習。
